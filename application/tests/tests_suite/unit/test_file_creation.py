87e13cff8f2865d4bf892ea02826ef541a008aba
Tomasz Budny <budny.tomasz@gmail.com>
2020-01-24 23:58:49 +0100

    tests: rename TestAddingFile to TestFileCreation

--- application/tests/tests_suite/unit/test_file_creation.py
+++ application/tests/tests_suite/unit/test_file_creation.py
@@ -5,13 +5,12 @@ from app.domain.entities.file import File
 from app.domain.value_objects.file_type import FileType
 
 
-class TestAddingFile(TestCase):
+class TestFileCreation(TestCase):
     def test_file_types(self):
         file_type = FileType('')
-
         self.assertIsInstance(obj=file_type, cls=Enum)
 
     def test_empty_file(self):
         file = File(content='', type_=FileType(''))
-
         self.assertIsInstance(obj=file, cls=File)
+
